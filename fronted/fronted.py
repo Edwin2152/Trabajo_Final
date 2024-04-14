e1d79118be7c743f58118fac4b16d0bfa0f31bd3
Edwin2152 <eaar2152@gmail.com>
2024-04-14 13:12:31 -0500

    sadsa
    sdas

--- fronted/fronted.py
+++ fronted/fronted.py
@@ -7,10 +7,12 @@ layout=dbc.Container([
     dbc.Row([
         dbc.Col("izquierda",md=2,style={'background-color':'blue'}),
         dbc.Col("derecha",md=10,style={'background-color':'black'}),
-
+html.Br(),html.Br(),html.Br(),html.Br(),
+        dbc.Col("izquierda",md=6,style={'background-color':'yellow'}),
+        dbc.Col("izquierda",md=6,style={'background-color':'blue'}),
     ])
 
 
 
-
 ])
+
