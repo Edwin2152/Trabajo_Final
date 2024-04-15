c0de666898f7e58fa688259fd2c82b7adaca9413
TatianaMejia8 <lesly.ptm@hotmail.com>
2024-04-14 20:49:38 -0500

    Izquierda

--- fronted/Estructuras/izquierda.py
+++ fronted/Estructuras/izquierda.py
@@ -1,9 +1,13 @@
 import dash
 import dash_bootstrap_components as dbc
 from dash import html
-
+from dash.dependencies import Input, Output
 
 izquierda=dbc.Container([
-
-    html.H1("cuadros")
+    dbc.Row([
+        dbc.Col(html.H2("Seleccione marca"), md=12, style={'background-color': '#155F82','margin-top':'15px'}),
+        dbc.Col(html.H2("Seleccione la referencia"), md=12, style={'background-color': '#155F82','margin-top':'15px'}),
+        dbc.Col(html.H2("Seleccione el modelo"), md=12, style={'background-color': '#155F82','margin-top':'15px'}),
+        dbc.Col(html.H4("Cotizar"), md=12, style={'background-color': '#155F82','margin-top':'15px'}),
+    ])    
 ])
--- fronted/fronted.py
+++ fronted/fronted.py
@@ -1,26 +1,23 @@
-import dash
-import dash_bootstrap_components as dbc
 from dash import html
+import dash_bootstrap_components as dbc
 
 
-#importamos enlaces de otras carpetas
-from.Estructuras.izquierda import izquierda
-from.Estructuras.derecha import derecha
+# importamos enlaces de otras carpetas
+from .Estructuras.izquierda import izquierda
+from .Estructuras.derecha import derecha
+from .Estructuras.inferior import inferior
 
-layout=dbc.Container([
+
+layout = dbc.Container([
     dbc.Row([
-        dbc.Col("logo",md=2,style={'background-color':'blue'}),
-        dbc.Col("nombre",md=10,style={'background-color':'purple'}),
-
-html.Br(),html.Br(),html.Br(),html.Br(),
-        dbc.Col(izquierda,md=6,style={'background-color':'yellow'}),
-        dbc.Col(derecha,md=6,style={'background-color':'gray'}),
-        
-    html.Br(),html.Br(),html.Br(),html.Br(),html.Br(),html.Br(),html.Br(),html.Br(),html.Br(),html.Br(),html.Br(),html.Br(),html.Br(),html.Br(),html.Br(),html.Br(),html.Br(),html.Br(),html.Br(),
-
+        dbc.Col(html.H2("Logo"), md=2, style={'background-color': '#E97132'}),
+        dbc.Col(html.H1("Cotizador de vehículos"), md=10,
+                style={'background-color': '#155F82'}),
+        html.Br(), html.Br(), html.Br(), html.Br(),
+        dbc.Col(izquierda, md=6, style={'background-color': '#DCEAF7'}),
+        dbc.Col(derecha, md=6, style={'background-color': 'white'}),
+        html.Br(), html.Br(), html.Br(), html.Br(), html.Br(), html.Br(), html.Br(), html.Br(), html.Br(), html.Br(
+        ), html.Br(), html.Br(), html.Br(), html.Br(), html.Br(), html.Br(), html.Br(), html.Br(), html.Br(),
+        dbc.Col(inferior, md=12, style={'background-color': '#155F82'}),
     ])
-
-
-
 ])
-
