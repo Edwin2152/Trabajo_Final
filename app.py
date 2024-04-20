b123b1dbfa55462317603a76dc3bd8cb396affda
Edwin2152 <eaar2152@gmail.com>
2024-04-20 08:18:26 -0500

    asdas

--- app.py
+++ app.py
@@ -1,14 +1,40 @@
 import dash
 import dash_bootstrap_components as dbc
+from dash import Dash, html,dcc, callback, Input, Output
+from pymongo.mongo_client import MongoClient
 
+
+from backend.base import db
 
 app= dash.Dash(__name__,external_stylesheets=[dbc.themes.BOOTSTRAP])
 
 from fronted.fronted import layout
 
-
 app.layout=layout
 
+@callback(
+    Output(),
+    Input("Marca_consultado", "value"),
+    Input("Referencia_consultado", "value"),
+    Input("Modelo_consultado", "value")
+)
+
+def consulta(auto_consultado,Referencia_consultado,Modelo_consultado):
+    # Consulta concatenada
+    filtro = {
+        "Marca": auto_consultado,
+        "Referencia": Referencia_consultado,
+        "Modelo": Modelo_consultado
+    }
+
+    # Realizar la consulta a la base de datos
+    resultados = db.find(filtro)
+
+    # Convertir los resultados a una lista
+    print(dataframe(resultados))
+
+   resulatdo = dataframe["Precio"]
+    return
 
 if __name__=='__main__':
     app.run_server(debug=True)
--- fronted/Estructuras/izquierda.py
+++ fronted/Estructuras/izquierda.py
@@ -6,6 +6,11 @@ from dash.dependencies import Input, Output
 izquierda=dbc.Container([
     dbc.Row([
         dbc.Col(html.H2("Seleccione marca"), md=12, style={'background-color': '#155F82','margin-top':'15px'}),
+
+
+
+
+        
         dbc.Col(html.H2("Seleccione la referencia"), md=12, style={'background-color': '#155F82','margin-top':'15px'}),
         dbc.Col(html.H2("Seleccione el modelo"), md=12, style={'background-color': '#155F82','margin-top':'15px'}),
         dbc.Col(html.H4("Cotizar"), md=12, style={'background-color': '#155F82','margin-top':'15px'}),
