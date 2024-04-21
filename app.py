1e831f978ea2b29a9b14156568a508d8ca7bb305
Edwin2152 <eaar2152@gmail.com>
2024-04-21 12:11:06 -0500

    as

--- app.py
+++ app.py
@@ -12,29 +12,29 @@ from fronted.fronted import layout
 
 app.layout=layout
 
-@callback(
-    Output(),
-    Input("Marca_consultado", "value"),
-    Input("Referencia_consultado", "value"),
-    Input("Modelo_consultado", "value")
-)
+#@callback(
+    #Output(),
+    #Input("Marca_consultado", "value"),
+    #Input("Referencia_consultado", "value"),
+    #Input("Modelo_consultado", "value")
+#)
 
-def consulta(auto_consultado,Referencia_consultado,Modelo_consultado):
+#def consulta(auto_consultado,Referencia_consultado,Modelo_consultado):
     # Consulta concatenada
-    filtro = {
-        "Marca": auto_consultado,
-        "Referencia": Referencia_consultado,
-        "Modelo": Modelo_consultado
-    }
+    #filtro = {
+       # "Marca": auto_consultado,
+        #"Referencia": Referencia_consultado,
+        #"Modelo": Modelo_consultado
+   # }
 
     # Realizar la consulta a la base de datos
-    resultados = db.find(filtro)
+    #resultados = db.find(filtro)
 
     # Convertir los resultados a una lista
-    print(dataframe(resultados))
+    #print(dataframe(resultados))
 
-   resulatdo = dataframe["Precio"]
-    return
+   #resultado = dataframe["Precio"]
+    #return
 
 if __name__=='__main__':
     app.run_server(debug=True)
